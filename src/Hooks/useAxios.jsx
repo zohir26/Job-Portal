c946f725bbca8afdb8a3a62c56a3f32eb9c757b2
zohir <jahirraihan500@gmail.com>
2024-12-26 17:00:25 +0600

    final push

--- src/Hooks/useAxios.jsx
+++ src/Hooks/useAxios.jsx
@@ -1,10 +1,35 @@
 import axios from "axios";
-
-const axiosInstance = axios.create({
+import { useEffect } from "react";
+import useAuth from "./useAuth";
+import { useNavigate } from "react-router-dom";
+const navigate= useNavigate();
+const axiosInstance = axios.
+create({
     baseURL:'http://localhost:5000',
     withCredentials:true
 })
 const useAxios = () => {
+  const {}= useAuth();
+  
+    useEffect(()=>{
+       axiosInstance.interceptors.response.use(response =>{
+        return response
+       }, error=>{
+        console.log('error caught in interceptor')
+        if(error.status === 401 || error.status === 403){
+            console.log('logout the user')
+
+            signOutUser ()
+            .then(()=>{
+                //move to login page
+                navigate('/login')
+            })
+        }
+        return Promise.reject(error)
+       })
+    },[])
+    
+    
     return axiosInstance;
 };
 
---
+++ src/Hooks/useAxiosSecure.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const useAxiosSecure = () => {
+    return 
+};
+
+export default useAxiosSecure;
